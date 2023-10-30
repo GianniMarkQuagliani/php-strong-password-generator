83f7c10a9d46645f135412bd0da373c550dc512d
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-10-30 02:44:40 +0100

    add footer

---
+++ footer.php
@@ -0,0 +1,13 @@
+<?php if (!empty($password)) { ?>
+    
+        
+    <h2>La password generata è:</h2>
+    <div class="error my-3">
+        <p class="text-center fs-3 fw-bold"><?php echo $password ?></p>
+    </div>
+    <a href="index.php">Torna indietro</a>
+
+
+<?php } ?>
+</body>
+</html>
